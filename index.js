3da234989b65fddb5e11ba72544f3d465287dcf8
dluman <dluman@allegheny.edu>
2022-09-25 16:51:02 -0400

    Adding environment stuff

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const repoName = (repo) => {
+  data = repo.split("/");
+  return data.at(-1);
+}
+
 const run = async () => {
+  const repo = repoName(process.env.GITHUB_REPOSITORY);
+  const owner = process.env.GITHUB_REPOSITORY_OWNER
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-  const repo = core.getInput('GITHUB_REPOSITORY');
-  console.log(repo);
+  console.log(`${repo} ${owner}`);
 };
 
 run();
