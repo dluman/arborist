ed665e47ab8139708e114c30345c744b13491528
dluman <dluman@allegheny.edu>
2022-09-25 08:05:26 -0400

    Update action to run

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 const run = async () => {
   const token = core.getInput('GITHUB_TOKEN');
-  const octokit = github.getOctoKit(token);
+  const octokit = github.getOctokit(token);
   const repo = core.getInput('GITHUB_REPOSITORY');
   console.log(repo);
 };
