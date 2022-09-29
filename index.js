d28019a45bf823230e4db16b275a8c4dc46b8b72
dluman <dluman@allegheny.edu>
2022-09-29 11:37:43 -0400

    Updating with team endpoints

--- index.js
+++ index.js
@@ -5,19 +5,38 @@ const octokit = github.getOctokit(
   process.env.GITHUB_TOKEN
 );
 
-const collaborators = async (owner, repo) => {
+const contributors = async (owner, repo) => {
   let list = await octokit.rest.repos.listContributors({
     owner: owner,
     repo: repo
   });
-  return list;
-}
+  return await list;
+};
+
+const teams = async (org) => {
+  let teams = await octokit.rest.teams.list({
+    org
+  });
+  return await teams;
+};
+
+const getTeam = async (org, team) => {
+  let team = octokit.rest.teams.getByName({
+    owner,
+    team
+  });
+  return await team;
+};
 
 const run = async () => {
   // Get project context
   const repo = github.context.payload.repository.name;
   const owner = github.context.payload.repository.owner.login;
-  console.log(await collaborators(owner, repo));
+  const teams = teams(owner);
+  for(let team of teams) {
+    console.log(team);
+    console.log(getTeam(owner, teams));
+  }
 };
 
 run();
